975ac3922e57d853356792606c3fae739ff566d6
Diana Alejandra Pava A <dianaalejandrapavaa@Dianas-MacBook-Pro.local>
2025-09-01 17:25:19 -0500

    first commit

---
+++ WomanPeriodApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WomanPeriodApp
+//
+//  Created by Diana Alejandra Pava A on 1/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WomanPeriodApp/WomanPeriodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomanPeriodAppApp.swift
+//  WomanPeriodApp
+//
+//  Created by Diana Alejandra Pava A on 1/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WomanPeriodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
